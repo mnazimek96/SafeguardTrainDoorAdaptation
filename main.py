79258adea30f488b8dfdfb999e44cf8c1989bf30
mnazimek96 <zimek142@icloud.com>
2020-12-18 08:17:00 +0100

    fix to exit

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
         elif choice == 'exit':
             exit = True
             print('=========================')
+            break
         else:
             print('Wrong input!')
             continue
